f0eadca68bf8c40197add74c4a394c004d4274cf
Snootic <snooticonoob@hotmail.com>
2025-09-03 21:03:33 -0300

    feat: update MainMenu layout and integrate GameSelect component

--- src/pages/mainMenu/index.tsx
+++ src/pages/mainMenu/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, Input } from '../../components'
+import { Button, Input, GameSelect } from '../../components'
 import './index.css'
+import type { Host } from '../../types';
 
 export default function MainMenu() {
 
@@ -11,14 +12,29 @@ export default function MainMenu() {
     setShowRoomsVisible(!showRoomsVisible);
   };
 
+  let [games, setGames] = React.useState<Host[]>([])
+  let [selectedGame, setSelectedGame] = React.useState<Host['address'] | undefined>(undefined)
+
+  React.useEffect(() => {
+    setGames([
+      { address: 'host1', name: 'Gallery One' },
+      { address: 'host2', name: 'Gallery Two' },
+      { address: 'host3', name: 'Gallery Three' }
+    ]);
+  }, []);
+
   return (
     <main>
       <div className='menu'>
-        <Button event={() => null} text={"Host a Gallery"} />
-        <Button event={handleShowRooms} text={"Join a Gallery"} />
+        <Button event={() => null} text={"Host a Gallery"} size='50%'/>
+        <Button event={handleShowRooms} text={"Join a Gallery"} size='50%'/>
         {showRoomsVisible && (
-          <div>
-            <Input value={host} setValue={setHost}/>
+          <div style={{ display: 'flex', flexDirection: 'column', width: '50%', gap: '5px' }}>
+            <div style={{display: 'flex', flexDirection: 'row', gap: 2}}>
+              <Input value={host} setValue={setHost}/>
+              <Button event={() => null} text={"Join"} size='35%'/>
+            </div>
+            <GameSelect values={games} selectedValue={selectedGame} setSelectedvalue={setSelectedGame}/>
           </div>
         )}
       </div>
