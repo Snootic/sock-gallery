710f7e338fc18c905a7cb50aed7da923f29a32aa
Snootic <snooticonoob@hotmail.com>
2025-09-03 21:02:54 -0300

    feat: update button css and add size prop

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,13 +4,15 @@ import './index.css'
 type buttonProps = {
   text: string
   event: () => void
+  size?: string
 }
 
 export const Button: React.FC<buttonProps> = (props) => {
   return (
-    <button 
+    <button
       onClick={props.event}
       className="button"
+      style={{width: props.size ?? "100%"}}
     >
       <p>{props.text}</p>
     </button>
