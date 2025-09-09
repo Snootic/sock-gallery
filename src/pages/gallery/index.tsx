d5bdd389720d5789b8083e020204b471cf53fbf6
snootic <kaik.eu19@gmail.com>
2025-09-09 18:01:49 -0300

    feat: refactor Gallery component to use SceneContent for improved structure and rendering

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,27 +1,105 @@
-import { MeshesProvider } from '../../hooks/meshesContext';
-import { Canvas } from '@react-three/fiber'
-import { useParams } from 'react-router-dom';
-import { useRef } from 'react';
-import { Box, Container } from '@mui/material';
-import { useTheme, type Theme } from '@mui/material/styles';
-import { Player, MeshObject } from '../../components'
-import { useSocket } from '../../hooks/useSocket';
-import PlayerBody from '../../components/player/body';
+import { MeshesProvider } from "../../hooks/meshesProvider";
+import { Canvas } from "@react-three/fiber";
+import { useParams } from "react-router-dom";
+import { useRef } from "react";
+import { Box, Container } from "@mui/material";
+import { useTheme, type Theme } from "@mui/material/styles";
+import { Player, MeshObject } from "../../components";
+import { useSocket } from "../../hooks/useSocket";
+import PlayerBody from "../../components/player/body";
+import { Frame, Lamp } from "../../components/blocks";
+import type { Socket } from "socket.io-client";
+import type { Player as PlayerType } from "../../types";
+
+const SceneContent = ({
+  socket,
+  players,
+}: {
+  socket: Socket;
+  players: PlayerType[];
+}) => {
+
+  const sceneBox = [
+    {
+      // chao
+      rotation: [-Math.PI / 2, 0, 0],
+      position: [0, -0.5, 0],
+      geometry: [50, 50],
+      color: "red",
+    },
+    {
+      //teto
+      rotation: [-Math.PI / 2, 0, 0],
+      position: [0, 6, 0],
+      geometry: [50, 50],
+      color: "blue",
+    },
+    {
+      rotation: [0, -Math.PI / 2, 0],
+      position: [25, 2, 0],
+      geometry: [50, 12],
+      color: "green",
+    },
+    {
+      rotation: [0, Math.PI / 2, 0],
+      position: [-25, 2, 0],
+      geometry: [50, 12],
+      color: "yellow",
+    },
+    {
+      rotation: [0, Math.PI, 0],
+      position: [0, 2, 25],
+      geometry: [50, 12],
+      color: "pink",
+    },
+    {
+      rotation: [0, 0, Math.PI],
+      position: [0, 2, -25],
+      geometry: [50, 12],
+      color: "orange",
+    },
+  ];
+
+  return (
+    <>
+      <group name={"scene-box"}>
+        {sceneBox.map((props, idx) => (
+          <MeshObject
+            key={idx}
+            rotation={props.rotation as [number, number, number]}
+            position={props.position as [number, number, number]}
+          >
+            <planeGeometry args={props.geometry as [number, number, number]} />
+            <meshStandardMaterial color={props.color} />
+          </MeshObject>
+        ))}
+      </group>
+      <ambientLight intensity={0.5} />
+      <Lamp position={[0,3,0]} />
+      {socket?.id && (
+        <Player id={socket.id as string} position={[0, 0, 0]} socket={socket} />
+      )}
+      {Object.entries(players).map(([id, playerData]) => (
+        <PlayerBody key={id} position={playerData.position} />
+      ))}
+
+      <Frame color={"blue"} position={[0, 0.5, 0]} />
+    </>
+  );
+};
 
 function Gallery() {
   const theme = useTheme();
-  const styles = getStyles(theme)
+  const styles = getStyles(theme);
   const { roomId } = useParams();
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const {socket, room, players} = useSocket(roomId)
+  const { socket, room, players } = useSocket(roomId);
 
   return (
     <Box component="main">
       <Container sx={styles.ui}>
-        <Box>
-          Current Room: {room ? room.id : "..."}
-        </Box>
+        <Box>Current Room: {room ? room.id : "..."}</Box>
       </Container>
       <MeshesProvider>
         <Canvas
@@ -32,24 +110,13 @@ function Gallery() {
               canvasRef.current.requestPointerLock();
             }
           }}
+          shadows={"soft"}
         >
-          <ambientLight intensity={Math.PI / 2} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-          {socket?.id &&
-            <Player id={socket.id as string} position={[0,0,0]} socket={socket}/>
-          }
-          {Object.entries(players).map(([id, playerData]) => (
-            <PlayerBody key={id} position={playerData.position}/>
-          ))}
-          <MeshObject rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
-            <planeGeometry args={[50, 50]} />
-            <meshStandardMaterial color="#e0e0e0" />
-          </MeshObject>
+          <SceneContent socket={socket!} players={players} />
         </Canvas>
       </MeshesProvider>
     </Box>
-  )
+  );
 }
 
 const getStyles = (theme: Theme) => ({
@@ -58,12 +125,12 @@ const getStyles = (theme: Theme) => ({
     zIndex: 100,
     top: 0,
     // userSelect: "none",
-    color: theme.palette.text.primary
+    color: theme.palette.text.primary,
   },
   canvas: {
-    height: '100vh',
-    width: '100vw'
-  }
-})
+    height: "100vh",
+    width: "100vw",
+  },
+});
 
-export default Gallery
+export default Gallery;
