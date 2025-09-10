8584180df606e283c2efb10cf78913e53eed71ba
Snootic <snooticonoob@hotmail.com>
2025-09-09 21:24:11 -0300

    feat: update Lamp component to enforce position prop and improve structure

--- src/components/blocks/lamp.tsx
+++ src/components/blocks/lamp.tsx
@@ -1,30 +1,46 @@
+import { SpotLight } from "@react-three/drei";
 import React from "react";
-import { Vector3 } from "three";
+import { Shape, Vector3 } from "three";
 
 type LampProps = {
-  position?: [number, number, number] | Vector3;
+  position: [number, number, number] | Vector3;
 };
 
 export const Lamp: React.FC<LampProps> = ({ position }) => {
+  const posArr: [number, number, number] =
+    Array.isArray(position)
+      ? position
+      : [position.x, position.y, position.z];
+
   return (
-    <>
-      <mesh position={position}>
-        <coneGeometry args={[1, 3, 1]} />
+    <group name="lamp">
+      <mesh position={posArr}>
+        <cylinderGeometry args={[0.05, 0.195, 0.4]} />
         <meshBasicMaterial color={"blue"} />
       </mesh>
-      <spotLight
-        castShadow
-        position={position}
-        angle={Math.atan(80 / 5)}
-        distance={70}
-        intensity={3}
-        decay={0}
-        penumbra={1}
-        shadow-mapSize={[2048, 2048]}
-        shadow-bias={-0.0001}
-        shadow-camera-near={1}
-        shadow-camera-far={5}
-      />
-    </>
+      <mesh
+        position={[posArr[0], posArr[1] - 0.2, posArr[2]]}
+        rotation={[Math.PI, 0, 0]}
+      >
+        <sphereGeometry args={[0.2, 10, 5, 0, 2 * Math.PI, 0, 0.5 * Math.PI]} />
+        <meshStandardMaterial emissive="white" emissiveIntensity={20} />
+        <SpotLight
+          castShadow
+          position={[0, 0, 0]}
+          radiusTop={0.2}
+          radiusBottom={10}
+          angle={Math.atan(80 / 5)}
+          distance={70}
+          intensity={3}
+          decay={0}
+          penumbra={1}
+          volumetric
+          shadow-mapSize={[2048, 2048]}
+          shadow-bias={-0.0001}
+          shadow-camera-near={1}
+          shadow-camera-far={5}
+        />
+      </mesh>
+    </group>
   );
 };
