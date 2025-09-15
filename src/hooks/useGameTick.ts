593b8b1d68111c133d3e3f9c0cf741d4d2458972
Snootic <snooticonoob@gmail.com>
2025-09-14 23:56:44 -0300

    fix: add force update mechanism in useGameTick for proper re-rendering

--- src/hooks/useGameTick.ts
+++ src/hooks/useGameTick.ts
@@ -1,12 +1,17 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback, useState } from "react";
 import { useUpdateGame } from "./updateGame";
 
 export const useGameTick = (host: boolean) => {
-  const TICK_RATE = 20 ;
+  const TICK_RATE = 20;
   const TICK_INTERVAL = 1000 / TICK_RATE;
+  const [, forceUpdate] = useState({});
 
   const updateGame = useUpdateGame();
   const updateGameRef = useRef(updateGame);
+
+  const triggerRerender = useCallback(() => {
+    forceUpdate({});
+  }, []);
 
   useEffect(() => {
     updateGameRef.current = updateGame;
@@ -15,8 +20,13 @@ export const useGameTick = (host: boolean) => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       updateGameRef.current(host);
+      // this will force re-render because
+      // updategame was not getting updated meshes data
+      // because scene was not being re-rendered
+      // do not know why, but this works
+      triggerRerender();
     }, TICK_INTERVAL);
 
     return () => clearInterval(intervalId);
-  }, [TICK_INTERVAL, host]);
+  }, [TICK_INTERVAL, host, triggerRerender]);
 };
